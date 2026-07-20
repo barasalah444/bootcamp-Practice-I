7fb96780bcda91e378e34c0b06ba5bbfaba7c9a5
barasalah444 <barahamdi2001@gmail.com>
2026-07-20 19:46:09 +0300

    Add files via upload

---
+++ Practice I.js
@@ -0,0 +1,36 @@
+// 1
+let favoriteAnimal = "Cat";
+console.log(favoriteAnimal);
+
+// 2
+let a = 5;
+let b = 10;
+console.log(a + b);
+
+// 3
+let age = 25;
+console.log(`I am ${age} years old.`);
+
+// 4
+let country = "Japan";
+country = "Canada";
+console.log(country);
+
+// 5
+const pi = 3.14;
+console.log(pi);
+
+// 6
+let favoriteColor = "Blue";
+console.log(favoriteColor);
+
+// 7
+let x = 5;
+let y = 10;
+console.log(x + y);
+
+// 8
+let isSunny = true;
+if (isSunny) {
+  console.log("It is sunny today.");
+}
